6526df3aeec4026307977e27d60c16939961c355
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-05-21 16:05:21 -0300

    tp5 ej2 added

---
+++ rowDaBoat/Userland/SampleCodeModule/printScreen.c
@@ -0,0 +1,9 @@
+char* buffer = (char*)0xB8000;
+
+/*
+1. Hacer que funcione imprimir una string
+2. Hacer que salte de linea si llega al final de la fila
+3. Hacer que salte de linea si llega un salto de linea en el string
+4. Implementar un segundo printScreen que reciba un unico color
+*/
+
--- rowDaBoat/Userland/SampleCodeModule/sampleCodeModule.c
+++ rowDaBoat/Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,13 +1,24 @@
-/* sampleCodeModule.c */
+// Necesito hacer que si encuentro un \n haga el salto
+// Ademas necesito verificar que si llego al final de una fila tmb haga el salto
+// Consta de 80 columnas y 25 filas
 
-char * buffer = (char*)0xB8000;
+#define BUFFERADDRESS (char*) 0xB8000
+char * bufferPointer = BUFFERADDRESS;
+
+int printScreen(char* string, char* color);
 
 int main() {
-    *buffer = 'A';
-    *(buffer+1) = 0x74;
-
-    *(buffer+2) = 'R';
-    *(buffer+3) = 0x56;
-
+    char* string = "hola arquitectura de la compu";
+    char color[] = {0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70,0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70, 0x70};
+    printScreen(string, color);
 	return 0xFAFAFA;
 }
+
+int printScreen(char* string, char* color){
+    for(int i = 0; string[i]; i++){
+        *(bufferPointer++) = string[i];
+        *(bufferPointer++) = color[i];
+    }
+    return 0;
+}
+
