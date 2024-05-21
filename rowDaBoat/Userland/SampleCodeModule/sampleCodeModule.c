32e662915486763e8273a018200b404d39db9489
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-05-21 14:45:26 -0300

    hola keo

--- rowDaBoat/Userland/SampleCodeModule/sampleCodeModule.c
+++ rowDaBoat/Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,19 +1,13 @@
 /* sampleCodeModule.c */
 
-char * v = (char*)0xB8000 + 79 * 2;
-
-static int var1 = 0;
-static int var2 = 0;
-
+char * buffer = (char*)0xB8000;
 
 int main() {
-	//All the following code may be removed 
-	*v = 'X';
-	*(v+1) = 0x74;
+    *buffer = 'A';
+    *(buffer+1) = 0x74;
 
-	//Test if BSS is properly set up
-	if (var1 == 0 && var2 == 0)
-		return 0xDEADC0DE;
+    *(buffer+2) = 'R';
+    *(buffer+3) = 0x56;
 
-	return 0xDEADBEEF;
+	return 0xFAFAFA;
 }
