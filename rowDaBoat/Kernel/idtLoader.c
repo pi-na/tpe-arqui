32970cc39821d8704154682628a2d019148562c5
Tomas Pinausig <tpinausigcastillo@itba.edu.ar>
2024-05-22 18:31:22 -0300

    anda el timertick holy fucking shit
    Co-authored-by: Javier Peral <za0sec@users.noreply.github.com>

--- rowDaBoat/Kernel/idtLoader.c
+++ rowDaBoat/Kernel/idtLoader.c
@@ -23,14 +23,12 @@ DESCR_INT * idt = (DESCR_INT *) 0;	// IDT de 255 entradas
 static void setup_IDT_entry (int index, uint64_t offset);
 
 void load_idt() {
-
   setup_IDT_entry (0x20, (uint64_t)&_irq00Handler);
   setup_IDT_entry (0x00, (uint64_t)&_exception0Handler);
 
 
 	//Solo interrupcion timer tick habilitadas
-	picMasterMask(0xFE); 
-	picSlaveMask(0xFF);
+	picMasterMask(0xFE);
         
 	_sti();
 }
