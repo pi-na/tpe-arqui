fdce58cc24e34c6fc587a8b76c9a33dc767ea064
za0sec <javiperalb@gmail.com>
2024-06-04 15:48:30 -0300

    DELETE SANCHU

--- rowDaBoat/santio/Kernel/include/sound.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOUND_H
-#define SOUND_H
-#include <stdint.h>
-
-void playSound(uint32_t frequence);
-void mute();
-void beep(uint32_t freq);
-
-#endif
--- rowDaBoat/santio/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Set audio device variable
-
-if [ "$(uname)" == "Darwin" ]; then
-    audio="coreaudio"
-else
-    audio="pa"
-fi
-sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -soundhw pcspk
